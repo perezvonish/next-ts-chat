370561695651c15f12d7f22cdfe75c34b49768fb
perezvonish <shokilak@gmail.com>
2022-10-30 20:28:11 +0600

    feat: rooms page

--- frontend/pages/rooms/index.tsx
+++ frontend/pages/rooms/index.tsx
@@ -1,9 +1,16 @@
+import {Col, Row, Statistic} from "antd";
+import styles from '../../styles/rooms.module.css'
+import RoomPreview from "../../components/RoomBlock/RoomPreview";
+
 export default function Rooms() {
     return (
-        <div>
-            <h1>
-                Room PAGE
-            </h1>
-        </div>
+        <Row className={styles.room_page}>
+            <Col span={8}>
+                <Statistic title="Rooms available" value={15} />
+            </Col>
+            <Col span={16}>
+                <RoomPreview/>
+            </Col>
+        </Row>
     )
 }
